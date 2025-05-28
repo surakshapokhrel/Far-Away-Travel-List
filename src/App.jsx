292a65effe3928ab92af789436a5ded7a662df27
suraksha pokhrel <suraksha2060@gmail.com>
2025-05-28 14:24:38 +0545

    Feat:Building a Form and Handling Submission

--- src/App.jsx
+++ src/App.jsx
@@ -17,10 +17,22 @@ function Logo() {
   return <h1>🌴 Far Away 💼</h1>;
 }
 function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
   return (
-    <div className="add-form">
+    <Form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-    </div>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>ADD</button>
+    </Form>
   );
 }
 function PackingList() {
